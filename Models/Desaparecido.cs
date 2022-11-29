27c3209ddec9acc3fadf39f70340568edeafdb8e
daneilhpp <carvalhosilvad88@gmail.com>
2022-11-28 21:32:13 -0300

    Upload

--- Models/Desaparecido.cs
+++ Models/Desaparecido.cs
@@ -6,28 +6,20 @@ namespace CarteiraVacinaca.Models
 {
     public class Desaparecido
     {
-        public Desaparecido(int id, string nome, int idEspecie, int idRaca, DateTime dtDesaparecimento, string lugar, string dono, string telefone, int idAnimal, string foto)
+        public Desaparecido(int id, DateTime? dtDesaparecimento, string lugar, string dono, string telefone, int idAnimal)
         {
             this.Id = id;
-            this.Nome = nome;
-            this.IdEspecie = idEspecie;
-            this.IdRaca = idRaca;
             this.DtDesaparecimento = dtDesaparecimento;
             this.Lugar = lugar;
             this.Dono = dono;
             this.Telefone = telefone;
             this.IdAnimal = idAnimal;
-            this.Foto = foto;
         }
         public int Id { get; set; }
-        public string Nome { get; set; }
-        public int IdEspecie { get; set;}
-        public int IdRaca { get; set; }
-        public DateTime DtDesaparecimento { get; set; }
+        public DateTime? DtDesaparecimento { get; set; }
         public string Dono { get; set; }
         public string Lugar { get; set; }
         public string Telefone { get; set; }
         public int IdAnimal { get; set; }
-        public string Foto { get; set; }
     }
 }
