f7392855808d15d79a2688117a3ebdf77f6a05b4
daneilhpp <carvalhosilvad88@gmail.com>
2022-12-08 16:27:36 -0300

    Versão Apresentação

--- Models/Animal.cs
+++ Models/Animal.cs
@@ -7,7 +7,7 @@ namespace carteiravacina.Models
     
     public class Animal
     {
-        public Animal(int id, string nome, int idEspecie, int idRaca, int idSexo, DateTime dtNascimento, string pelagem, int idSituacao, double peso, DateTime dataAdicao)
+        public Animal(int id, string nome, int idEspecie, int idRaca, int idSexo, DateTime? dtNascimento, string pelagem, int idSituacao, double peso, DateTime? dataAdicao)
         {
             this.Id = id;
             this.Nome = nome;
@@ -25,11 +25,11 @@ namespace carteiravacina.Models
         public int IdEspecie { get; set;}
         public int IdRaca { get; set; }
         public int IdSexo { get; set; }
-        public DateTime DtNascimento { get; set; }
+        public DateTime? DtNascimento { get; set; }
         public string Pelagem { get; set; }
         public int IdSituacao { get; set; }
         public double Peso { get; set; }
-        public DateTime DataAdicao { get; set; }
+        public DateTime? DataAdicao { get; set; }
         
     }
 }
